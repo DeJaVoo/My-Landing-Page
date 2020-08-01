a7665e6c11990e0d3757da88ae174adb6bcf6bc2
Dvir Segal <DeJaVoo@users.noreply.github.com>
2020-08-01 22:22:53 +0300

    Updates

---
+++ precache-manifest.21fcbba9cb9d9f0db41275d09a1f478c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bca51570deaffa26752d",
+    "url": "/dvir-segal/static/js/runtime~main.1470aa00.js"
+  },
+  {
+    "revision": "703731037d0f3cdb1563",
+    "url": "/dvir-segal/static/js/main.4b51b180.chunk.js"
+  },
+  {
+    "revision": "3ca7e8bbd4d1cd5ff20f",
+    "url": "/dvir-segal/static/js/2.365522a5.chunk.js"
+  },
+  {
+    "revision": "703731037d0f3cdb1563",
+    "url": "/dvir-segal/static/css/main.498e2056.chunk.css"
+  },
+  {
+    "revision": "f6af561c82ad2fbf6b7f1c7df03003c5",
+    "url": "/dvir-segal/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Dvir-Segal/precache-manifest.612dc2759fdac8cf44021cdfce66f0cb.js"
+  "/dvir-segal/precache-manifest.21fcbba9cb9d9f0db41275d09a1f478c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Dvir-Segal/index.html", {
+workbox.routing.registerNavigationRoute("/dvir-segal/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
