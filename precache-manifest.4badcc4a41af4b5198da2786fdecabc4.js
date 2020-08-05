43be87b8b3404d8741b261da45045cac145ac445
Dvir Segal <DeJaVoo@users.noreply.github.com>
2020-08-05 10:54:04 +0300

    Updates

--- precache-manifest.4badcc4a41af4b5198da2786fdecabc4.js
+++ precache-manifest.4badcc4a41af4b5198da2786fdecabc4.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/dvir-segal/static/js/runtime~main.1470aa00.js"
   },
   {
-    "revision": "7c10be8e89d384cb75bb",
-    "url": "/dvir-segal/static/js/main.33350b8d.chunk.js"
+    "revision": "79e6541ffa7bb61dc49e",
+    "url": "/dvir-segal/static/js/main.b1404c0a.chunk.js"
   },
   {
     "revision": "3ca7e8bbd4d1cd5ff20f",
     "url": "/dvir-segal/static/js/2.365522a5.chunk.js"
   },
   {
-    "revision": "7c10be8e89d384cb75bb",
+    "revision": "79e6541ffa7bb61dc49e",
     "url": "/dvir-segal/static/css/main.889ac71e.chunk.css"
   },
   {
-    "revision": "33cc129d122e286e36cf5d8832cbff6e",
+    "revision": "ca0d3fa26771f381dd3cf129a0602be4",
     "url": "/dvir-segal/index.html"
   }
 ];
