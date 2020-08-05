a888136a714693ec33fa64ebab9c1cab22712afa
Dvir Segal <DeJaVoo@users.noreply.github.com>
2020-08-05 11:36:06 +0300

    Updates

--- precache-manifest.4a04e02e99350ee4f903036b53b73c8e.js
+++ precache-manifest.4a04e02e99350ee4f903036b53b73c8e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/dvir-segal/static/js/runtime~main.1470aa00.js"
   },
   {
-    "revision": "69554a73e95f21244bd4",
-    "url": "/dvir-segal/static/js/main.edc0bab0.chunk.js"
+    "revision": "b527608a69b322f2a6ea",
+    "url": "/dvir-segal/static/js/main.491cc9b0.chunk.js"
   },
   {
     "revision": "3ca7e8bbd4d1cd5ff20f",
     "url": "/dvir-segal/static/js/2.365522a5.chunk.js"
   },
   {
-    "revision": "69554a73e95f21244bd4",
+    "revision": "b527608a69b322f2a6ea",
     "url": "/dvir-segal/static/css/main.889ac71e.chunk.css"
   },
   {
-    "revision": "4deb5cd25f8bc6031f272a5bc5b96051",
+    "revision": "3975697d1e7771ec361b0d065769abc5",
     "url": "/dvir-segal/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dvir-segal/precache-manifest.a16d577a79f48b739b23a578128b1237.js"
+  "/dvir-segal/precache-manifest.4a04e02e99350ee4f903036b53b73c8e.js"
 );
 
 workbox.clientsClaim();
